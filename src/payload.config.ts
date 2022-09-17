1feda75ac0f21aede8dba05beee3ac62a2451cde
Shivam Mishra <scm.mymail@gmail.com>
2022-09-17 10:37:51 +0530

    feat: SEO

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,8 @@ import Organisation from './collections/Organisation';
 import Individual from './collections/Individual';
 import Principles from './globals/principles';
 
+import seo from '@payloadcms/plugin-seo';
+
 export default buildConfig({
   serverURL: 'http://localhost:3000',
   admin: {
@@ -20,4 +22,11 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
+  plugins: [
+    seo({
+      collections: ['blog'],
+      globals: ['principles'],
+      tabbedUI: true,
+    }),
+  ],
 });
