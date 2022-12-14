c3ffb66fa48025357a4e4994e6241b89b90f453f
Shivam Mishra <scm.mymail@gmail.com>
2022-12-14 14:14:48 +0530

    feat: load script for authors

--- data/load.py
+++ data/load.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 import requests
 
 
@@ -7,6 +8,9 @@ def load_csv(filename):
     with open(filename, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         return [row for row in reader]
+
+
+data = load_csv("blog-export.csv")
 
 
 def make_post(base, json_data):
@@ -19,21 +23,53 @@ def make_post(base, json_data):
         json=json_data,
     )
     print(response.status_code)
-    print(response.content)
+
+    return response
 
 
-def load_blogs():
-    data = load_csv("blog-export.csv")
+def load_blogs(authors):
     for row in data:
+        author_array = [author.strip() for author in row["name"].split(", ")]
+        author_ids = [authors[auth] for auth in author_array]
+
         json_data = {
             "title": row["title"],
             "slug": row["slug"],
             "_status": "published",
-            "authorString": row["name"],
+            "author": author_ids,
             "publishDate": row["publish_date"],
         }
 
         make_post("blog", json_data)
 
 
-load_blogs()
+def load_authors():
+    author_strings = [row["name"] for row in data]
+    split_comma_strings = [string.split(",") for string in author_strings]
+    authors = [
+        string.strip() for string in itertools.chain.from_iterable(split_comma_strings)
+    ]
+
+    match_list = {
+        "Apar Gupta": "Apar",
+        "Akash Kumar Singh": "Akash Singh",
+        "SOP Volunteers": "SaveOurPrivacy Volunteers",
+        "Joanne DCunha": "Joanne D'Cunha",
+    }
+
+    unique_authors = list(set(authors))
+    author_ids = {}
+
+    for author in unique_authors:
+        if author not in match_list.values():
+            response = make_post("authors", {"name": author})
+            author_id = response.json().get("doc", {}).get("id")
+            author_ids[author] = author_id
+            if author in match_list.keys():
+                author_ids[match_list[author]] = author_id
+
+    return author_ids
+
+
+authors = load_authors()
+load_blogs(authors)
